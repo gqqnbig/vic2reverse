496d361b35fb93cec2865f3843c9a9ab394539e6
QQ喵 <gqqnb2005@gmail.com>
2023-07-15 16:43:42 +0800

    add ConsoleCommand struct

--- v2game.exe.h
+++ v2game.exe.h
@@ -9,3 +9,12 @@ struct Country
   char unknown[424];
   int plurality; //minimum is 1000, maximum is 100000.
 };
+
+//First undefine fields in IDA assembly view, then right-click on the first field,
+//choose this struct.
+struct ConsoleCommand
+{
+  const char *explanation;
+  void *handler;
+  char numberOfArguments;
+};
